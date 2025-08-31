61a008c7dcc69d7b9167dba265874256f3916b40
Somto Sabastine <sommysabudeh@gmail.com>
2025-08-31 01:34:29 +0100

    chore: restore PostgreSQL and Redis wait logic in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# echo "Waiting for postgres..."
-# while ! nc -z db 5432; do
-#   sleep 0.5
-# done
-# echo "PostgreSQL started"
+echo "Waiting for postgres..."
+while ! nc -z db 5432; do
+  sleep 0.5
+done
+echo "PostgreSQL started"
 
-# echo "Waiting for redis..."
-# while ! nc -z redis 6379; do
-#   sleep 0.5
-# done
-# echo "Redis started"
+echo "Waiting for redis..."
+while ! nc -z redis 6379; do
+  sleep 0.5
+done
+echo "Redis started"
 
 # Run migrations
 python manage.py migrate --noinput
