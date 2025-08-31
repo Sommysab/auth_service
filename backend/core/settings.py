c4fe18dec23cd04570f5637410beee3a55fe952b
Somto Sabastine <sommysabudeh@gmail.com>
2025-08-31 15:00:31 +0100

    chore: update .gitignore to exclude backend users admin files and comment out static directory in settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -88,7 +88,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    # BASE_DIR / 'static',  # static directory doesn't exist
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
